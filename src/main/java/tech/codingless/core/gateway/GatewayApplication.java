0330bc2933094c040424eb9e93b4b0b2e63ea88d
wanghongyan <258156979@qq.com>
2022-09-12 19:53:54 +0800

    跨域

--- src/main/java/tech/codingless/core/gateway/GatewayApplication.java
+++ src/main/java/tech/codingless/core/gateway/GatewayApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 
@@ -21,6 +25,20 @@ public class GatewayApplication {
 		// GracefulShutdownCallback
 	}
 
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 1、配置跨域
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+        // 2、设置哪些路径需要跨域
+        source.registerCorsConfiguration("/**",corsConfiguration);
 
+        return new CorsWebFilter(source);
+    }
+ 
 
 }
